4a7f94e45d1dcaa36c6b7f149cb5e485b5cde809
jeremy.gau <caliban3010@gmail.com>
2020-02-26 17:04:55 +0100

    RollBack avant ajout de la réinitialisation Changement de l'icone du cactus

--- game.js
+++ game.js
@@ -1,9 +1,8 @@
-const terrain = document.getElementById('terrain');
-const dimTerrain = terrain.getBoundingClientRect();
+const dimTerrain = document.getElementById('terrain').getBoundingClientRect();
 const right_border = dimTerrain.width;
 const bottom_border = dimTerrain.height;
 let indexObstacle = 0;
-let objects = [];
+let objects;
 let dino;
 let global_speed;
 let jump;
@@ -14,7 +13,28 @@ let score;
 let highScore = 0;
 let timer;
 let obstacleCreation;
-let gameOn = false;
+
+function place_objects(objects) {
+	for(let i = 0; i < objects.length; i++) {
+		var object = objects[i];
+		var element = document.getElementById(object.id);
+		element.style.left = object.x + "px";
+		element.style.top = object.y + "px";
+	}
+}
+
+function update() {
+	out();
+	contact();
+	jumping();
+	document.getElementById('score').innerText = parseInt(score).toString();
+	global_speed *= 1.0001;
+	for (let i = 1; i < objects.length; i++) {
+		objects[i].x -= global_speed;
+	}
+	score += global_speed;
+	place_objects(objects);
+}
 
 function Dino() {
 	this.id = "dino";
@@ -37,8 +57,8 @@ function addObstacle(height, width) {
 	let element = document.createElement('img');
 	let newId = 'obstacle' + indexObstacle;
 	element.id = newId;
-	element.src = 'cactus.svg';
-	terrain.appendChild(element);
+	element.src = 'img/cactus.svg';
+	document.getElementById('terrain').appendChild(element);
 	var object = new Obstacle(height, width, newId);
 	objects.push(object);
 	indexObstacle++;
@@ -46,28 +66,6 @@ function addObstacle(height, width) {
 	element.style.top = object.y + "px";
 	element.style.position = 'absolute';
 	element.style.height = object.height+ "px";
-}
-
-function update() {
-	out();
-	jumping();
-	document.getElementById('score').innerText = parseInt(score).toString();
-	global_speed *= 1.0001;
-	for (let i = 1; i < objects.length; i++) {
-		objects[i].x -= global_speed;
-	}
-	score += global_speed;
-	place_objects(objects);
-	contact();
-}
-
-function place_objects(objects) {
-	for(let i = 0; i < objects.length; i++) {
-		var object = objects[i];
-		var element = document.getElementById(object.id);
-		element.style.left = object.x + "px";
-		element.style.top = object.y + "px";
-	}
 }
 
 function jumping() {
@@ -89,11 +87,6 @@ function event_handler(event) {
 		jumpTime = 0;
 		dino.vy = dino_speed;
 	}
-	if (event.key === " " && !gameOn) {
-		gameOn = true;
-		clearGame();
-		init();
-	}
 }
 
 function vitesse(t) {
@@ -102,7 +95,7 @@ function vitesse(t) {
 
 function out() {
 	if (objects[1].x < 0) {
-		terrain.removeChild(document.getElementById(objects[1].id));
+		document.getElementById('terrain').removeChild(document.getElementById(objects[1].id));
 		objects.splice(1,1);
 	}
 }
@@ -112,25 +105,18 @@ function contact() {
 		&& objects[1].y < dino.y + dino.height) {
 		clearInterval(timer);
 		clearInterval(obstacleCreation);
-		gameOn = false;
-		if (score > highScore) {
-			highScore = parseInt(score);
-		}
 	}
-}
-
-function clearGame() {
-	for (let i = 1; i < objects.length; i++)  {
-		terrain.removeChild(document.getElementById(objects[i].id));
+	if (score > highScore) {
+		highScore = score;
 	}
-	document.getElementById('highScore').innerText = highScore;
 }
 
 function init() {
 	indexObstacle = 0;
 	dino_speed = 15;
 	jumpingTime = dino_speed * 2 + 1;
-	global_speed = 5;
+	global_speed = 3;
+	objects = [];
 	score = 0;
 	jump = false;
 	dino = new Dino();
@@ -142,3 +128,5 @@ function init() {
 	}, 1000);
 	document.getElementById('highScore').innerText = highScore;
 }
+
+init();
